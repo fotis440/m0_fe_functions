06fa55c43f2056f4e897d150d969b981e9160839
Fotis Kousiakis <fkousiakis@gmail.com>
2023-09-30 20:50:32 -0500

    taskHalfComplete

--- cleanUp.js
+++ cleanUp.js
@@ -1,44 +1,61 @@
 // Each example below has at least one opportunity for improvement.
 
 // YOUR TASK:
-  // Modify the code to make that improvement(s)
-  // write a JS comment to explain what you changed, and why
-  // make sure the code you submit WORKS - you can run this entire file 
-    // using `node cleanUp.js` or copy and paste into a replit
+// Modify the code to make that improvement(s)
+// write a JS comment to explain what you changed, and why
+// make sure the code you submit WORKS - you can run this entire file 
+// using `node cleanUp.js` or copy and paste into a replit
 
 
 // EX 1:
-function askForName() { console.log("Hello, what is your name?") }
+//function askForName() { console.log("Hello, what is your name?")
+//                      }
 
-askForName()
+//askForName();
 
+console.log("Hello, what is your name?");
 
 // EX 2:
 function addThreeNums(first, second, third) {
-var sum = first + second + third
-return sum;
-      }
+  var sum = first + second + third
+  return sum;
+}
 
-addThreeNums(1, 2, 3);
-addThreeNums(4, 2, 7);
+console.log(addThreeNums(1, 2, 3));
+console.log(addThreeNums(4, 2, 7));
+//addThreeNums(1, 2, 3);
+//addThreeNums(4, 2, 7);
 
 
 // EX 3:
-func makeFreshPesto(){
-  console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
-  console.log("Pulse basil and pine nuts");
-  console.log("Add garlic and cheeses");
-  console.log("Slowly pour in oil");
-  console.log("Season");    }
 
-makeFreshPesto();
+console.log(
+  "Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper\n" +
+  "Pulse basil and pine nuts\n" +
+  "Add garlic and cheeses\n" +
+  "Slowly pour in oil\n" +
+  "Season"
+);
+
+// function makeFreshPesto(){
+//   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
+//   console.log("Pulse basil and pine nuts");
+//   console.log("Add garlic and cheeses");
+//   console.log("Slowly pour in oil");
+//   console.log("Season");    }
+
+// makeFreshPesto();
 
 
 //  EX 4:
-function average(num1, num2) 
-  {
-var sum = num1 + num2;
-    var avg = sum / 2;
+// function average(num1, num2) 
+//   {
+// var sum = num1 + num2;
+//     var avg = sum / 2;
 
-return avg
-  }
+// return avg
+//   }
+
+function average(num1, num2) {
+  return (num1 + num2) / 2;
+}
